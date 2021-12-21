f9fd9dd8f55960bc9d62f45d3e414f4054774313
ZhouYao <1607435943@qq.com>
2021-12-21 20:11:03 +0800

    Initial Commit

---
+++ Mybatis_07_mbg/src/main/java/com/atguigu/mybatis/test/MyBatisTest.java
@@ -0,0 +1,63 @@
+package com.atguigu.mybatis.test;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ @author：ZhouYao
+ @create：2021-12-16 13:19
+ */
+public class MyBatisTest {
+
+    public SqlSessionFactory getSqlSessionFactory() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        return new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    @Test
+    public void testMbg() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("E:/IDEA/workspace_idea1/MyBatis/Mybatis_07_mbg/mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Mybatis_10_plugin/src/main/java/com/atguigu/mybatis/dao/EmployeeMapper.java
@@ -0,0 +1,13 @@
+package com.atguigu.mybatis.dao;
+
+import com.atguigu.mybatis.bean.Employee;
+
+/**
+ @author：ZhouYao
+ @create：2021-12-09 19:25
+ */
+public interface EmployeeMapper {
+
+    Employee getEmpById(Integer id);
+
+}
